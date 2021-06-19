e210b354bf10e4c1e4c1b1abd75ae4bb2df2f5e2
Ransara <ransarawijitha@gmail.com>
2021-06-20 01:01:22 +0530

    Read RSSI value and publish to MQTT topic, BL and WiFi simultaneuous operation success

---
+++ code/scanner/scanner.ino
@@ -0,0 +1,116 @@
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+
+const char* ssid     = "malmi_villa";
+const char* password = "86467223E";
+const char* mqtt_server = "test.mosquitto.org";
+int rssi = 0;
+char rssi_buf[5];
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+//SETUP WIFI
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+// MQTT CONNECTION ////////////////////////////////////////////////////////////////////////////////////////
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "EN2560Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("ransara69", "hello world");
+      // ... and resubscribe
+      client.subscribe("ransara699");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+
+//BLE SCAN /////////////////////////////////////////////////////////////////////////////////////////////////
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+      rssi = advertisedDevice.getRSSI();
+      Serial.println(rssi);
+      snprintf(rssi_buf,sizeof(rssi_buf),"%d",rssi);
+      client.publish("ransara69", rssi_buf);
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  //client.setCallback(callback);
+
+  Serial.println("Scanning BLE beacons...");
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //BEGIN MQTT CONNECTION 
+    if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  //
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(2000);
+}
